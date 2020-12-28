3c40375867e737d25516f069fd2341c31058ef41
yangjiangdong <aiaoyang@outlook.com>
2020-12-28 11:37:39 +0800

    优化代码

--- apis/apis.go
+++ apis/apis.go
@@ -12,57 +12,21 @@ func GetResource() {
 }
 
 func GetECS() (string, error) {
-	ecsinfo, err := aliyun.GetECS()
-	if err != nil {
-		return "", err
-	}
-	res, err := json.Marshal(ecsinfo)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%s", res), nil
+	return errHandler(aliyun.GetECS())
 }
 func GetRDS() (string, error) {
-	rdsinfo, err := aliyun.GetRDS()
-	if err != nil {
-		return "", err
-	}
-	res, err := json.Marshal(rdsinfo)
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%s", res), nil
+	return errHandler(aliyun.GetRDS())
 }
 
 // func GetLifeTime() (string, error) {
 // 	return aliyun.GetLifeTime()
 // }
 func GetAlarm() (string, error) {
-	alarm, err := aliyun.GetAlarm()
-	if err != nil {
-		return "", err
-	}
-
-	res, err := json.Marshal(alarm)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%s", res), nil
+	return errHandler(aliyun.GetAlarm())
 
 }
 func GetDomain() (string, error) {
-	domain, err := aliyun.GetDomain()
-	if err != nil {
-		return "", err
-	}
-
-	res, err := json.Marshal(domain)
-	if err != nil {
-		return "", err
-	}
-
-	return fmt.Sprintf("%s", res), nil
+	return errHandler(aliyun.GetDomain())
 }
 
 func GetRedis() {
@@ -71,4 +35,21 @@ func GetRedis() {
 
 func GetDelay() {
 
+}
+
+func GetCert() (string, error) {
+	return errHandler(aliyun.GetCertList())
+}
+
+func errHandler(payload interface{}, err error) (res string, rerr error) {
+	if err != nil {
+		rerr = err
+		return
+	}
+	tmpRes, rerr := json.Marshal(payload)
+	if rerr != nil {
+		return
+	}
+	res = fmt.Sprintf("%s", tmpRes)
+	return
 }
